11c523e98759db23d80e3f5cd89f9e4cfdbd2ab0
Yuiko Grace Nakano <gracenak@gmail.com>
2020-07-22 19:59:15 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,10 +7,10 @@ class Shoe
   def initialize(brand)
     @brand = brand
     if BRANDS include?(brand) 
-      else
-        BRANDS << brand 
-        end
-      end
+    else
+      BRANDS << brand 
+    end
+  end
 
   def cobble
     self.condition = "new"
