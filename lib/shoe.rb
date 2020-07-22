49d4739a3b5d9fb9e5e6254fced1ebe895a1bd92
Yuiko Grace Nakano <gracenak@gmail.com>
2020-07-22 19:44:16 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,7 +2,7 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
   
-  BRANDS = []
+  BRANDS.size = []
 
   def initialize(brand)
     @brand = brand
@@ -15,6 +15,6 @@ class Shoe
   
   def brand=(brand)
     @brand = brand
-    BRANDS << brand 
+    BRANDS.size << brand 
   end
 end
